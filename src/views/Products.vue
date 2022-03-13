c1c50dd7a11e31319d294e4fd5775555e860f5ed
AOtenkaya <otenkayadev@gmail.com>
2022-03-13 23:54:06 +0300

    made product page logic and style

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,13 +1,37 @@
 <template>
-  <v-layout row wrap>
-
+  <v-layout row wrap justify-center>
+    <v-flex xs8>
+      <v-layout row wrap>
+        <v-flex v-for="(product, index) in productList" :key="index" xs12 md6 lg4 pa-4>
+          <item-card :item="product"/>
+        </v-flex>
+      </v-layout>
+    </v-flex>
   </v-layout>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
+import ItemCard from '../components/ItemCard.vue';
+
 export default {
   name: 'Products',
   components: {
+    ItemCard,
+  },
+  data() {
+    return {
+      productList: [],
+    };
+  },
+  created() {
+    this.fetchProductList()
+      .then(({ data }) => {
+        this.productList = data;
+      });
+  },
+  methods: {
+    ...mapActions(['fetchProductList']),
   },
 };
 </script>
