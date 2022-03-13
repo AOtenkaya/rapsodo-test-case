768bbb141ce86e4736eb841cf8ef94083e739361
AOtenkaya <otenkayadev@gmail.com>
2022-03-13 11:38:45 +0300

    added vue-notifications package for snackbars

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
+import Notifications from 'vue-notification';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
 
+Vue.use(Notifications);
 Vue.config.productionTip = false;
 
 new Vue({
