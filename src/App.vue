d5b803a0cac2de00beb90de793e5ee58fdafaa5e
AOtenkaya <otenkayadev@gmail.com>
2022-03-13 19:50:50 +0300

    transferred toolbar to seperate component

--- src/App.vue
+++ src/App.vue
@@ -1,69 +1,23 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="#232021"
-      dark
-    >
-      <v-layout row wrap align-center>
-        <v-flex shrink>
-          <v-img
-            alt="rapsodo-logo"
-            class="shrink mr-2"
-            contain
-            src="./assets/logos/rapsodo-logo.png"
-            transition="scale-transition"
-            width="40"
-          />
-        </v-flex>
-
-        <v-flex shrink>
-          <h1 class="ml-2">Rapsodo Store</h1>
-        </v-flex>
-
-        <v-spacer/>
-
-        <v-flex shrink>
-          <router-link to="/" class="no-text-decoration">
-            <v-btn
-              class="mx-2"
-              fab
-              text
-              icon
-            >
-              <v-icon>
-                mdi-store
-              </v-icon>
-            </v-btn>
-          </router-link>
-
-          <router-link to="/basket" class="no-text-decoration">
-            <v-btn
-              class="mx-2"
-              fab
-              text
-              icon
-            >
-              <v-icon>
-                mdi-cart
-              </v-icon>
-            </v-btn>
-          </router-link>
-        </v-flex>
-      </v-layout>
-    </v-app-bar>
+    <app-toolbar/>
 
     <v-main>
       <router-view/>
     </v-main>
+
+    <notifications position="bottom right" :close-on-click="true" />
   </v-app>
 </template>
 
 <script>
+import AppToolbar from './components/AppToolbar.vue';
 
 export default {
   name: 'App',
-
+  components: {
+    AppToolbar,
+  },
   data: () => ({
   }),
 };
---
+++ src/components/AppToolbar.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-app-bar
+      app
+      color="#232021"
+      dark
+    >
+      <v-layout row wrap align-center>
+        <v-flex shrink>
+          <v-img
+            alt="rapsodo-logo"
+            class="shrink mr-2"
+            contain
+            src="../assets/logos/rapsodo-logo.png"
+            transition="scale-transition"
+            width="40"
+          />
+        </v-flex>
+
+        <v-flex shrink>
+          <h2 class="ml-2">Rapsodo Store</h2>
+        </v-flex>
+
+        <v-spacer/>
+
+        <v-flex shrink>
+          <router-link to="/" class="no-text-decoration">
+            <v-btn
+              class="mx-2"
+              fab
+              text
+              icon
+            >
+              <v-icon>
+                mdi-store
+              </v-icon>
+            </v-btn>
+          </router-link>
+
+          <router-link to="/basket" class="no-text-decoration">
+            <v-btn
+              class="mx-2"
+              fab
+              text
+              icon
+            >
+              <v-icon>
+                mdi-cart
+              </v-icon>
+            </v-btn>
+          </router-link>
+        </v-flex>
+      </v-layout>
+    </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: 'AppToolbar',
+};
+</script>
+
+<style>
+
+</style>
