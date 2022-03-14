f287b07f09216e7fd303c4d0be84cb126acfa12b
AOtenkaya <otenkayadev@gmail.com>
2022-03-14 16:41:22 +0300

    cleared unused getters file

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,6 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import state from './state';
 import actions from './actions';
-import getters from './getters';
 import mutations from './mutations';
 
 Vue.use(Vuex);
@@ -10,6 +9,5 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state,
   actions,
-  getters,
   mutations,
 });
