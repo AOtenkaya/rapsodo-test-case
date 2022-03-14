aceeb18fcda10a036ac313c7c3077f456537cfe0
AOtenkaya <otenkayadev@gmail.com>
2022-03-14 16:42:29 +0300

    made style improvement on item card

--- src/components/ItemCard.vue
+++ src/components/ItemCard.vue
@@ -6,32 +6,38 @@
           <v-img
             alt="item-photo"
             src="https://rapsodocertified.b-cdn.net/wp-content/uploads/2020/01/ipad-and-hitting-monitor-1024x768.png"
-            width="300"
-            class="item-image"
+            max-width="240"
+            max-height="240"
+            class="centerized"
+            lazy
           />
         </v-flex>
 
         <v-divider/>
 
-        <v-flex xs4 class="text-start pa-2">
+        <v-flex xs4 :class="isBasketItem ? 'centerized' : ''" text-start pa-2>
           <h2 class="py-1 px-6 bold">{{ item.name }}</h2>
           <h4 class="py-1 px-6">Size: {{ item.size }}</h4>
           <h4 class="py-1 px-6">Color: {{ item.color }}</h4>
-          <h3 class="py-1 px-6 orange--text">${{ item.amount }}</h3>
+          <h3 class="py-1 px-6">${{ item.amount }}</h3>
         </v-flex>
 
-        <v-flex xs4>
-          <v-layout :class="isBasketItem ? 'column' : 'row'" justify-center align-center>
+        <v-flex v-if="calculateItemQuantity(item)" xs4>
+          <v-layout
+            :class="isBasketItem ? 'column' : 'row'"
+            justify-center align-center fill-height
+          >
             <v-flex shrink>
-              <v-btn text block left color="black">
+              <v-btn text block left color="black" @click="decreaseQuantity(item)">
                   <v-icon>
                     mdi-minus
                   </v-icon>
                 </v-btn>
             </v-flex>
 
-            <v-flex md4 xs6 pa-2>
+            <v-flex md4 xs6 pa-2 :class="isBasketItem ? 'basket-quantity' : ''">
               <v-text-field
+                :value="calculateItemQuantity(item)"
                 name="quantity"
                 type="number"
                 color="black"
@@ -42,7 +48,7 @@
             </v-flex>
 
             <v-flex shrink>
-              <v-btn text block right color="black" class="text-center">
+              <v-btn text block right color="black" @click="increaseQuantity(item)">
                   <v-icon>
                     mdi-plus
                   </v-icon>
@@ -50,14 +56,21 @@
             </v-flex>
           </v-layout>
         </v-flex>
+
+        <v-flex v-else xs4 pa-2>
+          <v-btn block @click="addToBasket(item)" class="no-text-transform">Add To Basket</v-btn>
+        </v-flex>
       </v-layout>
     </v-card-text>
   </v-card>
 </template>
 
 <script>
+import manageBasketItemsMixin from '../mixins/manageBasketItems';
+
 export default {
   name: 'ItemCard',
+  mixins: [manageBasketItemsMixin],
   props: {
     item: {
       type: Object,
@@ -74,7 +87,11 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.item-image {
+.centerized {
   margin: auto;
+}
+
+.basket-quantity {
+  width: 150px;
 }
 </style>
