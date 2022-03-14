0be52a790593f5b8fd0ee7e841b1de969bf1425a
AOtenkaya <otenkayadev@gmail.com>
2022-03-14 16:42:59 +0300

    wrote basket and basket summary components

---
+++ src/components/BasketSummary.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-card class="text-center">
+    <v-card-title class="gray-bg">
+      <v-layout row wrap>
+        <v-flex xs12 mx-2 px-4>
+          <h3>Basket Summary</h3>
+        </v-flex>
+      </v-layout>
+    </v-card-title>
+
+    <v-card-text>
+      <v-layout row wrap>
+        <v-flex xs12 py-9>
+          <h2>SubTotal: ${{ basketSubTotal }}</h2>
+        </v-flex>
+      </v-layout>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+  name: 'BasketSummary',
+  computed: {
+    ...mapState(['basketItems']),
+    basketSubTotal() {
+      const asd = this.basketItems.reduce((sum, basketItem) => {
+        const subTotal = sum + basketItem.amount * basketItem.quantity;
+
+        return subTotal;
+      }, 0);
+
+      return asd;
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
--- src/views/Basket.vue
+++ src/views/Basket.vue
@@ -1,13 +1,70 @@
 <template>
-  <v-layout row wrap>
+  <v-layout row wrap justify-center pa-4>
+    <v-flex md8 xs12 pa-2>
+      <v-card class="align-center">
+        <v-card-title class="pr-6 gray-bg">
+          <h3 class="black--text">Products</h3>
 
+          <v-spacer/>
+
+          <router-link to="/" class="no-text-decoration">
+            <v-icon class="mr-2 mb-1">mdi-keyboard-backspace</v-icon>
+            <span>Continue Shopping</span>
+          </router-link>
+        </v-card-title>
+
+        <v-card-text>
+          <v-layout v-if="basketItems.length" row wrap>
+            <v-flex xs12 v-for="(basketItem, index) in basketItems" :key="index" pa-2>
+              <item-card :item="basketItem" is-basket-item/>
+            </v-flex>
+          </v-layout>
+
+          <v-layout v-else row wrap py-4 align-center justify-center>
+            <v-flex grow>
+              <h4>
+                There is no item on basket yet you can go to our
+                store and add products to your basket
+              </h4>
+            </v-flex>
+
+            <v-flex shrink class="mx-4 text-center">
+              <router-link to="/" class="no-text-decoration">
+                <v-btn
+                  outlined
+                  large fab
+                  color="black"
+                >
+                  <v-icon>
+                    mdi-store
+                  </v-icon>
+                </v-btn>
+              </router-link>
+            </v-flex>
+          </v-layout>
+        </v-card-text>
+      </v-card>
+    </v-flex>
+
+    <v-flex md4 xs12 pa-2>
+      <basket-summary/>
+    </v-flex>
   </v-layout>
 </template>
 
 <script>
+import { mapState } from 'vuex';
+import ItemCard from '../components/ItemCard.vue';
+import BasketSummary from '../components/BasketSummary.vue';
+
 export default {
   name: 'Basket',
   components: {
+    ItemCard,
+    BasketSummary,
+  },
+  computed: {
+    ...mapState(['basketItems']),
   },
 };
 </script>
